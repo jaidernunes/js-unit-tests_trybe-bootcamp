cac27e21210a659465cb6a8d8d0da034abf3411e
jaidernunes <jaidernf@gmail.com>
2022-08-18 17:10:52 -0300

    req 8 ok - 5/10?

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,13 +14,34 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
+// SOLUÇÃO ELIMINANDO LOOP:
+// const myCounter = () => {
+//   var myArray = [];
+//   for (var counter = 0; counter <= 3; counter += 1) {
+//     myArray.push(counter);
+//     myArray.push(2);
+//     myArray.push(3);
+//   }
+//   return myArray;
+// };
 
+// ORIGINAL
+// const myCounter = () => {
+//   var myArray = [];
+//   for (var counter = 0; counter <= 3; counter += 1) {
+//     myArray.push(counter);
+//     for (var counter = 2; counter <= 3; counter += 1) {
+//       myArray.push(counter);
+//     }
+//   }
+//   return myArray;
+// };
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
